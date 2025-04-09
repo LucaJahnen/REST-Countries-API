a2104e82c0ffa3df19c943f95b86515c2225de83
LucaJahnen <luca.jahnen@gmail.com>
2025-04-09 15:47:01 +0200

    adding navbar and search input

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-var message = 'Hello World';
-console.log(message);
+var form = document.querySelector(".search-form");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+});
 //# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,4 @@
+const form: HTMLFormElement = document.querySelector(".search-form");
+form.addEventListener("submit", e => {
+    e.preventDefault();
+})
