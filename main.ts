01c487ef3960fecfba25fe68671513e1ec37d18e
LucaJahnen <luca.jahnen@gmail.com>
2025-04-16 14:54:18 +0200

    adding detailed card for countries

--- main.ts
+++ main.ts
@@ -57,6 +57,8 @@ const makeRequest = async (query: string) => {
 }
 
 const getAllCountries = async () => {
+    root.innerHTML = "";
+    form.style.display = "flex";
     const resp = await makeRequest("all");
     resp.forEach(country => {
         const formattedCountry = formatResp(country);
@@ -112,12 +114,35 @@ interface FormattedCountry {
     capital: string;
 }
 
+interface FormattedDetailedCountry {
+    flag: string,
+    name: string,
+    nativeName: string,
+    population: string,
+    region: string,
+    subRegion: string,
+    capital: string[],
+    topLevelDomain: string[],
+    currencies: Object[],
+    languages: string,
+    borderCountries: string[]
+}
+
+interface NativeName {
+    common: string;
+}
+
 interface Response {
     flags: { svg: string };
-    name: { common: string };
+    name: { common: string, nativeName: NativeName[] };
     population: number;
     region: string;
-    capital: string;
+    subregion: string;
+    capital: string[];
+    borders: string[];
+    tld: string[];
+    currencies: Object[];
+    languages: Object[];
 }
 
 const showCountriesByRegion = async (query: string) => {
@@ -139,10 +164,30 @@ function formatResp(resp: Response): FormattedCountry {
     }
 }
 
+function formatDetailedResp(resp: Response[]): FormattedDetailedCountry {
+    const lastIndex = Object.values(resp[0].name.nativeName).length - 1
+    const nativeName = Object.values(resp[0].name.nativeName)[lastIndex].common
+
+    return {
+        flag: resp[0].flags.svg,
+        name: resp[0].name.common,
+        nativeName: nativeName,
+        population: resp[0].population.toLocaleString("en-US"),
+        region: resp[0].region,
+        subRegion: resp[0].subregion ?? "No data",
+        capital: resp[0].capital,
+        topLevelDomain: resp[0].tld ?? ["No data"],
+        currencies: Object.values(resp[0].currencies).map((currency: { name: string }) => currency.name),
+        // currencies: Object.values(resp[0].currencies),
+        languages: Object.values(resp[0].languages).join(", "),
+        borderCountries: resp[0].borders
+    }
+}
+
 const renderCard = async (obj: FormattedCountry) => {
     const template = `
     <section class="card">
-        <button class="tab-button">
+        <button class="tab-button" onclick='renderDetailedCountry("${obj.name}")'>
         <img class="card-img" src="${obj.flag}" alt="Flag of ${obj.name}" />
         <div class="text-wrapper">
             <h2 class="card-heading">${obj.name}</h2>
@@ -153,5 +198,51 @@ const renderCard = async (obj: FormattedCountry) => {
     </button>
    </section> 
    `
+
     root.innerHTML += template;
+}
+
+const renderDetailedCountry = async (name: string) => {
+    let obj = await makeRequest(`name/${name.toLowerCase()}?fullText=true`);
+    obj = formatDetailedResp(obj);
+    form.style.display = "none";
+
+    let borderCountries = obj.borderCountries ? await Promise.all(obj.borderCountries.map(async (borderCountry: string) => {
+            const countryName = await makeRequest("alpha/" + borderCountry);
+            const name = countryName[0].name.common;
+            return `<button onclick="renderDetailedCountry('${name}')">${name}</button>`;
+        })
+    ) : "No border countries"
+
+    if(Array.isArray(borderCountries)) {
+        borderCountries = borderCountries.join("");
+    }
+
+    const template = `
+    <div class="detailed-card">
+        <button class="back-button" onclick="getAllCountries()">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+            <span>Back</span>
+        </button>
+        <div class="main-card">
+            <img src="${obj.flag}" alt="Flag of ${obj.name}">
+            <section>
+                <h1>${obj.name}</h1>
+                <p>Native Name: ${obj.nativeName}</p>
+                <p>Top level domain: ${obj.topLevelDomain.join(", ")}</p>
+                <p>Population: ${obj.population}</p>
+                <p>Currencies: ${obj.currencies.join(", ")}</p>
+                <p>Region: ${obj.region}</p>
+                <p>Languages: ${obj.languages}</p>
+                <p>Sub Region: ${obj.subRegion}</p>
+                <p>Capital: ${obj.capital.join(", ")}</p>
+            </section>
+            <div>
+                <p><span>Border Countries:</span>${borderCountries}</p>
+            </div>
+        </div>
+    </div>
+    `
+
+    root.innerHTML = template;
 }
